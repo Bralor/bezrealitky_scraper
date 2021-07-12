13d862c9ed5275eb4157043ed8740fe15720b6f0
Matous <bralor92@email.cz>
2021-07-12 18:35:21 +0200

    scraper.py: importing modules & reading json added #2 #4

--- br_scraper/src/scraper.py
+++ br_scraper/src/scraper.py
@@ -1,36 +1,56 @@
 import os
 import json
+from importlib import import_module
 
 import requests
 
 
 class Scraper:
-    def __init__(self, url: str, json_files: str):
+    def __init__(self, url: str, source: str, location: str):
         self.url = url
-        self.json_files = json_files
+        self.source = source
+        self.location = location
+
+
+    def find_params(self) -> str:
+        if not os.path.isdir(self.source):
+            raise Exception(f"Folder: {self.source}, does not exist.")
+
+        return [
+            file
+            for file in os.listdir(self.source)
+            if os.path.splitext(file)[1] == ".py"
+            and os.path.splitext(file)[0] == self.location
+        ].pop()
+
+
+    def get_module_name(self, py_file: str) -> tuple:
+        return (
+            path := os.path.join(self.source, py_file),
+            os.path.splitext(path.replace(os.sep, ".", 2))[0]
+        )
+
+
+    @staticmethod
+    def load_params(path: str, modulename: str) -> dict:
+        if not os.path.isfile(path):
+            raise Exception(f"File \"{path}\" does not exists")
+
+        return import_module(modulename).request_params
 
 
     def send_request(self, params):
         if not params:
             raise Exception(f"Missing argument {params}")
 
-        self.response = requests.post(self.url, params)
-
-
-    def load_json(self, json_f):
-        if not isinstance(json_f, str):
-            raise Exception("Response object is not str")
-
-        self.content = json.loads(json_f)
+        self.response = requests.post(self.url, params).text
 
 
     @staticmethod
-    def find_params(path: str) -> list:
-        if not os.path.isdir(path):
-            raise Exception(f"Folder: {path}, does not exist.")
+    def read_json(str_json):
+        if not isinstance(str_json, str):
+            raise Exception("Response object is not str")
 
-        return [
-            file
-            for file in os.listdir(path)
-            if os.path.splitext(file)[1] == ".json"
-        ]
+        return json.loads(str_json)
+
+
