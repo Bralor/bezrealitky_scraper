570e69746d740e536bceb31b5892b72c7a18ff68
Matous <bralor92@email.cz>
2021-07-21 13:22:58 +0200

    db.py: small modification in wrapper added #8

--- br_scraper/src/db.py
+++ br_scraper/src/db.py
@@ -2,11 +2,12 @@ import pymongo
 
 
 class DbWrapper():
-    def __init__(self, host: str, port: str, db_name: str, col: str, data: list) -> None:
-        self.host = host
+    def __init__(self, hostname: str, port: int, db_name: str,
+                 col_name: str, data: list) -> None:
+        self.hostname = hostname
         self.port = port
         self.db_name = db_name
-        self.col = col
+        self.col_name = col_name
 
         if not data:
             raise Exception("There are no data to insert")
@@ -16,32 +17,55 @@ class DbWrapper():
 
     def connect_db(self) -> None:
         try:
-            self.client = pymongo.MongoClient(self.host, self.port)
+            self.client = pymongo.MongoClient(
+                self.hostname,
+                self.port
+            )
 
         except pymongo.errors.ServerSelectionTimeoutError as err:
             raise Exception(f"Cannot connect to the db ({err})")
         else:
-            if not self.db_exists():
+            if not self.db_exists() and not self.col_exists():
                 self.create_collection()
-                print(f"Collection {self.col} already exists")
             else:
                 self.db = self.client[self.db_name]
+                self.collection = self.db[self.col_name]
+                print("Using existing database and collection")
 
 
     def db_exists(self) -> bool:
         return self.db_name in self.client.list_database_names()
 
 
+    def col_exists(self) -> bool:
+        return self.col_name in self.client[self.db_name].list_collection_names()
+
+
     def create_collection(self) -> None:
         self.db = self.client[self.db_name]
-        self.collection = self.db[self.col]
-        print(f"Collection {self.col} created")
+        self.collection = self.db[self.col_name]
+        print(f"Collection {self.col_name} created")
 
+
+    def add_indexing(self, index_name: str) -> None:
+        self.collection.create_index(
+            [(index_name, pymongo.ASCENDING)],
+            unique=True
+        )
 
     def write_documents(self, data: list) -> None:
-        if not data:
-            raise Exception("Data object is empty")
+        if not data and not isinstance(data, list):
+            raise Exception("Argument 'data' is not list or is empty")
 
-        # missing indexing
-        for item in data:
-            self.db[self.db_name].insert_one(item)
+        self.collection.insert_many(data)
+
+
+    def read_documents(self) -> list:
+        if not self.collection:
+            raise Exception("Argument 'collection' is empty")
+
+        return [
+            document
+            for document in self.collection.find({})
+        ]
+
